58050a014c78a2ecca65a418166631fe0ac33db0
tam-truong <47429219+tam-truong@users.noreply.github.com>
2019-02-14 15:29:31 -0700

    Create ControllerInput.java

---
+++ Design/ControllerInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class ControllerInput {
+	private Scanner firstIn;
+
+	public void setFirstIn(Scanner input) {
+		this.firstIn = input;
+	}
+	public Scanner getFirstIn() {
+		return firstIn;
+	}
+}
