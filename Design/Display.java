6bcfaea0b547aa8e7dd21f9bb137ce181245f517
isaacsadeyemo <47429193+isaacsadeyemo@users.noreply.github.com>
2019-02-23 22:23:46 -0700

    Update Display.java

--- Design/Display.java
+++ Design/Display.java
@@ -1,14 +1,23 @@
-public class Display{
+public class Display extends Map {
+
     Map dis = new Map();
-    dis.grid
+    dis.Mapmethod();
 
+    private String[][] =new String [10][10];
 
+    public void printGrid() {
+        for (int i = 0; i < grid.length; i++) {
+            System.out.print((row % 10) + " ");
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.print(grid[i][j]);
+            }
+            System.out.print("\n");
 
-
-
-
-
-
-
-
+        }
+    }
+    System.out.print(" ");
+    for (int j = 0; j < 10; j++){
+        System.out.print((j % 10));
+    }
+    System.out.println();
 }
