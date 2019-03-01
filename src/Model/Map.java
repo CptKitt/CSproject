cb1341ce07ced114084c168c564448d5fdea587b
AianC <aechen0@gmail.com>
2019-03-01 12:22:11 -0700

    Added functionality to Map.
    getGrid(), possibleMovesForCharacter(Position), and processAction(Position, Position) are mostly functional and documented.

--- src/Model/Map.java
+++ src/Model/Map.java
@@ -4,14 +4,16 @@ import java.util.*;
 public class Map implements Pathfinding.Delegate {
 	private String[][] grid;
 	Random rand = new Random();
-
+	private Entity[][] entities;
 	private Position start = new Position(rand.nextInt(10),rand.nextInt(10));
 
 	public Map() {
-		grid = new String[10][10];
+		this(10, 10);
 	}
+	
 	public Map(int x,int y) {
 		grid = new String[x][y];
+		entities = new Entity[x][y];
 	}
 
 	public Position getStart() {
@@ -29,6 +31,17 @@ public class Map implements Pathfinding.Delegate {
 		start = new Position(
 				rand.nextInt(10),
 				rand.nextInt(10));
+	}
+	
+	public Entity[][] getGrid() {
+		Entity[][] copy = new Entity[entities.length][entities[0].length];
+		for (int x = 0; x < entities.length; x++) {
+			for (int y = 0; y < entities[x].length; y++) {
+				// TODO: create a new entity rather than copy reference
+				copy[x][y] = entities[x][y];
+			}
+		}
+		return entities;
 	}
 
 	public void populateGrid() {
@@ -58,6 +71,60 @@ public class Map implements Pathfinding.Delegate {
 			System.out.print("\n");
 		}
 	}
+	
+	/**
+	 * Calculates the possible moves for an Entity at a Position.
+	 * @param p The Position of the Entity.
+	 * @return A Set of Positions that the Entity can move to.
+	 */
+	public Set<Position> possibleMovesForCharacter(Position p) {
+		// no character at position, return empty set
+		if (entities[p.x][p.y] == null) {
+			return new HashSet<>();
+		}
+		
+		// TODO: use entity movement rather than number
+		return Pathfinding.movementForPosition(
+				this, p, 5);
+	}
+	
+	/**
+	 * Attempts to process an action.
+	 * @param p1 The Entity performing the action.
+	 * @param p2 The destination Position for the action.
+	 * @return Whether the action was valid or not.
+	 */
+	public boolean processAction(Position p1, Position p2) {
+		// no character selected, ignore
+		if (entities[p1.x][p1.y] == null) {
+			return false;
+		}
+		// destination is another character
+		else if (entities[p2.x][p2.y] != null) {
+			// not in melee range, return
+			if (p1.distanceTo(p2) != 1) {
+				return false;
+			}
+			else {
+				// TODO: ask entities to fight each other
+			}
+		}
+		// destination is an empty space
+		else {
+			// out of movement range, return
+			if (!possibleMovesForCharacter(p1).contains(p2)) {
+				return false;
+			}
+			else {
+				// move character
+				entities[p2.x][p2.y] = entities[p1.x][p1.y];
+				entities[p1.x][p2.y] = null;
+			}
+		}
+		
+		// action successfully completed
+		return true;
+	}
 
 	public void pathfind() {
 		Set<Position> moves = Pathfinding.movementForPosition(this,start,rand.nextInt(4)+2);
@@ -78,6 +145,7 @@ public class Map implements Pathfinding.Delegate {
 		} else if (p.y < 0 || p.y >= 10) {
 			return false;
 		}
+		// TODO: add entity wall checks
 		if (grid[p.x][p.y] == "#") {
 			return false;
 		}
