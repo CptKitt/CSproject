c7c378597f1c89778526fc2ff5afb4549251ae5e
AianC <aechen0@gmail.com>
2019-02-28 20:35:08 -0700

    Filled in GUIMain, fixed imports in other classes.
    Also changed drawMapOnScene in Display to use a Group rather than a Scene, since you can't do anything with a Scene.
    GUIMain can be run, and shows a blank window.

--- src/GUI/GUIMain.java
+++ src/GUI/GUIMain.java
@@ -1,15 +1,75 @@
 package GUI;
 
-public class GUIMain {
+import Model.*;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+/**
+ * JavaFX application representing the GUI
+ * interface for the game.
+ */
+public class GUIMain extends Application {
 	private Map map;
-	
+	private Display display;
+	private Input input;
+	private Scene scene;
+	private Group root;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// create i/o objects
+		display = new Display();
+		input = new Input();
+
+		// create map
+		map = new Map();
+		map.populateGrid();
+
+		// javafx setup
+		root = new Group();
+		scene = new Scene(root, 720, 480);
+
+		// set up event handlers
+		scene.addEventFilter(MouseEvent.MOUSE_PRESSED, this::sceneClicked);
+
+		// display once
+		display.drawMapOnScene(map, root);
+
+		// show application
+		primaryStage.setScene(scene);
+		primaryStage.setResizable(false);
+		primaryStage.setTitle("\"Sample Text Here\" Game");
+		primaryStage.show();
+	}
+
+	/**
+	 * Starting point to launch the application.
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	/**
+	 * Event handling function for clicks in the scene.
+	 * @param e The MouseEvent to process.
+	 */
+	private void sceneClicked(MouseEvent e) {
+		// delegate input handling
+		Position p = input.handleClick(e);
+
+		// TODO: send input to map and remove debug line
+		System.out.println("click at x:" + e.getSceneX()
+				+ ", y:" + e.getSceneY());
+
+		// clear and update display
+		root.getChildren().clear();
+		display.drawMapOnScene(map, root);
+	}
+
 	public boolean validInput(String s) {
 		return false;
 	}
-	public void start(Stage s) {
-	}
-	public void main(String[] args) {
-		
-	}
-
 }
