76e5c7fc1fc1e213fd138b962d7f7b596f589f73
tam-truong <47429219+tam-truong@users.noreply.github.com>
2019-03-02 17:32:59 -0700

    Suggestion for DIsplay
    Added a suggestion on implementing printGrid() without needed a Map instance variable

--- src/Text/Display.java
+++ src/Text/Display.java
@@ -5,6 +5,7 @@ package Text;
  */
 public class Display {
 
+	//A suggestion by Tam: maybe have printGrid take a Map object as parameter so that TextMain can pass through the Map variable that it contains. Then the printGrid method should store a 2d array through the getGrid() method that you could permutate.
     public void printGrid() {
 	for (int i = 0; i < grid.length; i++) {
 		System.out.print((row % 10) + " ");
