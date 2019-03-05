0d1c1743753bde8fb8fb4c9077ec38827657e247
tam-truong <47429219+tam-truong@users.noreply.github.com>
2019-03-04 21:18:05 -0700

    Update UserInput.java

--- src/Text/UserInput.java
+++ src/Text/UserInput.java
@@ -9,16 +9,20 @@ import java.util.Scanner;
 public class UserInput {
 	
 	/**
-	 *
+	 *This method asks the User for a position on the map displayed in the
+	 *Interactive Text-Based version in the form: x,y.
+	 *Has no parameters, Returns a Position Object.
 	 */
 	public Position movementInput() {
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("Please enter a position to move a character as an ordered pair, with a comma inbetween (e.g. x,y):");
+
 		String input = userInput.nextLine();
 		while (input.length() != 3) {
 			System.out.println("Please include the comma \",\" inbetween your x and y values");
 			input = userInput.nextLine();
 		}
+
 		int xCoord = Integer.parseInt(String.valueOf(input.charAt(0)));
 		int yCoord = Integer.parseInt(String.valueOf(input.charAt(2)));
 		Position inputPos = new Position(xCoord, yCoord);
