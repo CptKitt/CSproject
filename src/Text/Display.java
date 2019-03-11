00e233a367d782d39f537610130b9a12b4d22f20
isaacsadeyemo <47429193+isaacsadeyemo@users.noreply.github.com>
2019-03-10 19:49:23 -0600

    Update Display.java

--- src/Text/Display.java
+++ src/Text/Display.java
@@ -14,9 +14,10 @@ public class Display {
 
             for (int i = 0; i < grid.length; i++) {
                 System.out.print(i + "");
-
+                System.out.print("  ");
                 for (int j = 0; j < grid[i].length; j++) {
                     System.out.print(j + "");
+
                     entities = grid[i][j];
 
                 if (entities == null) {
@@ -35,6 +36,10 @@ public class Display {
 
 
         }
+    }
+    public static void main(String[] args){
+
+
     }
 }
 
