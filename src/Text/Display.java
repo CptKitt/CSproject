fca756ad04ec9b9d32d241660e69e549758bef57
isaacsadeyemo <47429193+isaacsadeyemo@users.noreply.github.com>
2019-03-10 18:59:00 -0600

    Update Display.java

--- src/Text/Display.java
+++ src/Text/Display.java
@@ -1,30 +1,40 @@
-package Text; 
+package Text;
+
+import Model.*;
 
 /**
  *
  */
 public class Display {
 
-	//A suggestion by Tam: maybe have printGrid take a Map object as parameter so that TextMain can pass through the Map variable that it contains. Then the printGrid method should store a 2d array through the getGrid() method that you could permutate.
-    public void printGrid() {
-	for (int i = 0; i < grid.length; i++) {
-		System.out.print((row % 10) + " ");
-		for (int j = 0; j < grid[i].length; j++) {
-			System.out.print(grid[i][j]);
-		}
-		System.out.print("\n");
-	}
-	System.out.print(" ");
-	for (int j = 0; j < 10; j++) {
-		System.out.print((j % 10));
-	}
-	System.out.println();
-    }
+    //A suggestion by Tam: maybe have printGrid take a Map object as parameter so that TextMain can pass through the Map variable that it contains. Then the printGrid method should store a 2d array through the getGrid() method that you could permutate.
+    public void printMap(Map map) {
+        Entity[][] grid = map.getGrid();
+        Entity entities;
 
-    public static void main (String[] args) {
-        boolean exit = false;
-        
+            for (int i = 0; i < grid.length; i++) {
+                System.out.print(i + "");
 
-}
+                for (int j = 0; j < grid[i].length; j++) {
+                    System.out.print(j + "");
+                    entities = grid[i][j];
+
+                if (entities == null) {
+                    System.out.println(" ");
+
+                } else if (entities instanceof Player) {
+                    System.out.println("x");
+
+                } else  {
+                    System.out.println("#");
+
+                }
+
+
+            }
+
+
+        }
     }
 }
+
