da7fc135f618f797c3623a939dd9144942e20e85
AianC <aechen0@gmail.com>
2019-03-14 10:46:42 -0600

    Fix null pointer bug.

--- src/Model/Map.java
+++ src/Model/Map.java
@@ -65,7 +65,9 @@ public class Map implements Pathfinding.Delegate {
 		Entity[][] copy = new Entity[getWidth()][getHeight()];
 		for (int x = 0; x < getWidth(); x++) {
 			for (int y = 0; y < getHeight(); y++) {
-				copy[x][y] = entities[x][y].copy();
+				if (entities[x][y] != null) {
+					copy[x][y] = entities[x][y].copy();
+				}
 			}
 		}
 		return copy;
