f2d56875d51308508254a00adbb598883d042f37
tam-truong <47429219+tam-truong@users.noreply.github.com>
2019-03-15 08:30:52 -0600

    Made changes to the alignment of x-axis labels in Display, and made it possible to exit out of the Game by giving an empty input using UserInput, also mad TextMain use a 20x10 map instead of 10x20

--- src/Text/Display.java
+++ src/Text/Display.java
@@ -7,7 +7,7 @@ import Model.*;
  */
 public class Display {
 
-    //A suggestion by Tam: maybe have printGrid take a Map object as parameter so that TextMain can pass through the Map variable that it contains. Then the printGrid method should store a 2d array through the getGrid() method that you could permutate.
+
     public void printMap(Map map) {
 
         Entity[][] grid = map.getGrid();
@@ -16,7 +16,11 @@ public class Display {
 
         System.out.print(" ");
         for (int i = 0; i < grid[0].length; i++) {
-            System.out.print("  " + i + " ");
+		if (i >= 0 && i < 10) {
+            		System.out.print("  " + i + " ");
+		} else {
+			System.out.print(" " + i + " ");
+		}
         }
 
 
