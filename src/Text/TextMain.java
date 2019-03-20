aa804ec739dd2cabc257a0c77f92d98907d0d7a1
tam-truong <47429219+tam-truong@users.noreply.github.com>
2019-03-20 11:35:59 -0600

    Update TextMain.java

--- src/Text/TextMain.java
+++ src/Text/TextMain.java
@@ -16,60 +16,14 @@ public class TextMain {
 
 	public static void main(String[] args) {
 
-		for (int fLevel = 0; fLevel < 4; fLevel++) {
-			if (floorType(fLevel)) {
-				for (int i = 0; i < 10; i++) {
-					map = new Map(10, 15);
+		map = new Map(10, 15);
 
-					Display display = new Display();
+		Display display = new Display();
 
-					UserInput input = new UserInput();
+		UserInput input = new UserInput();
 
-					while (true) {
-						display.printMap(map);
-						System.out.println("Floor Level: " + fLevel + i + " " + floor);
-						playerTurn(input);
-						
-					}
-				}
-			} else {
-				System.out.println("Congratulations! You have traversed all floors, and Conquered the Map");
-				break;
-			}
+		while (true) {
+			display.printMap(map);
+			playerTurn(map, input);
 		}
-	}
-
-	/**
-	* Currently Looks for the type of Floor for each threshold
-	*/
-	public static boolean floorType(int level) {
-		if (level == 0) {
-			floor = "Dungeon";
-			return true;
-		} else if (level == 1) {
-			floor = "Cave";
-			return true;
-		} else if (level == 2) {
-			floor = "Tower";
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	/**
-	*Used to disguish user/player turn order, and methods that user may use
-	*/
-	public static void playerTurn(UserInput input) {
-		//List<Player> players = levelMap.getPlayers();
-		Position move = input.movementInput();
-	}
-
-	/**
-	*Used to distinguis enemy turn and methods that enemyAI may use
-	*/
-	public static void computerTurn(Map levelMap) {
-		List<Enemy> enemies = levelMap.getEnemies();
-
-	}
 }
