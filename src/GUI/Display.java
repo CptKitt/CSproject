69a2781ce4846f0502160fa46cb21a4a29258c43
CptKitt <47429244+CptKitt@users.noreply.github.com>
2019-03-21 14:37:13 -0600

    Update Display.java
    added documentation

--- src/GUI/Display.java
+++ src/GUI/Display.java
@@ -12,6 +12,12 @@ import java.io.File;
 public class Display {
 	public static final double size = 32d;
 
+	/**Draw the map on the screen.
+	*@param m is a Map that gets drawn.
+	*@param g is a GraphicsContext used like a canvas to draw the map onto .
+	*@param highlighted is a set of positions used to draw the move overlay.
+	*
+	*/
 	public void drawMapOnScene(Map m, GraphicsContext g, Set<Position> highlighted) {
 		Entity[][] grid = m.getGrid();
 		double[][] visgrid = m.getVisibility();
@@ -34,29 +40,28 @@ public class Display {
 
 				Entity e = grid[i][j];
 
-				if (e instanceof Obstacle) {
-                    //"space" is a black box (representing where the wall isn't visible because of the roof)
-                    if (i<grid.length-1 && grid[i+1][j] instanceof Obstacle) {
-                        g.drawImage(space,j*size,i*size);
-                    }
-                    else {
-                        g.drawImage(wall,j*size,i*size);
-                    }
-                }
-				else if (e instanceof Stairs) {
+				if (e instanceof Obstacle) { //draw walls
+            if (i<grid.length-1 && grid[i+1][j] instanceof Obstacle) {
+                g.drawImage(space,j*size,i*size);
+            }
+            else {
+                g.drawImage(wall,j*size,i*size);
+            }
+        }
+				else if (e instanceof Stairs) { //draw stairs
 					g.drawImage(downstairs,j*size,i*size);
 				}
-				else if (e instanceof Enemy) {
+				else if (e instanceof Enemy) { //draw enemies
 					g.drawImage(slime,j*size,i*size);
 				}
-                else if (grid[i][j] instanceof Player) {
-                    g.drawImage(hero,j*size,i*size);
-                }
+        else if (grid[i][j] instanceof Player) {//draw players
+            g.drawImage(hero,j*size,i*size);
+        }
 
-				if (highlighted.contains(new Position(i,j))) {
+				if (highlighted.contains(new Position(i,j))) { //draw move overlay
 					g.drawImage(highlight,j*size,i*size);
 				}
-				else {
+				else { //draw visibility overlay (the "shroud" or fog of war)
 					g.setGlobalAlpha(1-visgrid[i][j]);
 					g.drawImage(shade,j*size,i*size);
 					g.setGlobalAlpha(1);
