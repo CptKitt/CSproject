a32ded9bf1d0510c8b6ee9c9beab27fe7cd639ae
tam-truong <47429219+tam-truong@users.noreply.github.com>
2019-04-02 16:01:29 -0600

    Basic Documentation for UserInput

--- src/Text/UserInput.java
+++ src/Text/UserInput.java
@@ -4,20 +4,21 @@ import Model.Position;
 import java.util.Scanner;
 
 /**
-*
-*/
+ * UserInput class asks and takes input from the user through the system terminal.
+ * Is implemented within TextMain to take input for the text-based version of Group 9's project.
+ * Contains methods: moveInput():Position.
+ */
 public class UserInput {
 	
 	/**
-	 *This method asks the User for a position on the map displayed in the
-	 *Interactive Text-Based version in the form: x,y.
-	 *@return a Position object containing coordinate information on where the user wants a character to move
+	 * The method moveInput() asks the User for a position on the map displayed in the Interactive Text-Based version in the form: x,y.
+	 * Parameters: none.
+	 * Returns: Position - contains coordinate information on where the user wants a character to move
 	 */
 	public Position moveInput() {
 		Scanner userInput = new Scanner(System.in);
 		System.out.println("");
 		System.out.println("Please enter a position as <x,y> to highlighted \"x\", or <> to pass, or <exit> to close game:");
-
 
 		while (true) {
 			try {
@@ -44,8 +45,8 @@ public class UserInput {
 	}
 
 	/**
-	*This main method is used for testing the compilation and functionality of methods within this class
-	*/
+	 * This main method is used for testing the compilation and functionality of methods within this class
+	 */
 	public static void main(String[] args) {
 		UserInput input = new UserInput();
 		Position test = new Position(input.moveInput());
