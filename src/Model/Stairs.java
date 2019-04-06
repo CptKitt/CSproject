c31e66380c148e758f39cd6bbd09fd41625972a1
tam-truong <47429219+tam-truong@users.noreply.github.com>
2019-04-06 15:08:18 -0600

    Documentation for Stairs

--- src/Model/Stairs.java
+++ src/Model/Stairs.java
@@ -1,9 +1,24 @@
 package Model;
 
+/**
+ * Stairs class inherits from Entity.
+ * Used to represent passable locales that will 
+ * allow the User to progress to new or old floors.
+ */
 public class Stairs extends Entity{
+
+  /**
+   * Default settings when creating a stairway.
+   */
   public Stairs() {
 		super(1000, 0, 1000, 0, null, 1);
 	}
+
+  /**
+   * copy() method that will type cast Stair object into parent Entity
+   * no parameters
+   * Returns an Entity class containing the same stats as current Stairs
+   */
   @Override
   public Entity copy() {
 		Stairs Stairs = new Stairs();
